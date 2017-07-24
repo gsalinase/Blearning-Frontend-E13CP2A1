9a6da079db44d14a5dcff2b3fafc68f50763d088
Gonzalo Salinas <gsalinase91@gmail.com>
2017-07-23 22:45:01 -0400

    Add piedra-papel-tijer folder

--- piedra_papel_tijera/js/main.js
+++ piedra_papel_tijera/js/main.js
@@ -1,16 +1,53 @@
-function colorear(piedra, papel, tijera){
-        pars = document.getElementsByTagName('img');
-        for (i = 0; i < pars.length; i++) {
-          pars[i].body.innerHTML = "piedra";
-          pars[i].style.backgroundColor = backgroundColor;
+function elemento1() {
+    document.getElementById("piedra").innerHTML = 'Piedra';
+}
+function elemento2() {
+    document.getElementById("papel").innerHTML = 'Papel';
+}
+function elemento3() {
+    document.getElementById("tijera").innerHTML = 'Tijera';
+}
+function win () {
+    document.getElementById("gana").innerHTML = compara(eleccionComputador);
+}
+var box = document.getElementsByClassName('box');
+var eleccionUsuario = box;
+var eleccionComputador = Math.random();
+if (eleccionComputador <0.34){
+    eleccionComputador = "Piedra";
+}else if(eleccionComputador <=0.67){
+    eleccionComputador = "Papel";
+}
+else{
+    eleccionComputador = "Tijera";
+}
+var compara = function(opcion1,opcion2){
+    if(opcion1===opcion2){
+        return "Empate";
+    }
+    if(opcion1==="Piedra"){
+        if(opcion2==="Tijera"){
+            return "Gana Piedra";
         }
-
-
-        var prob = Math.random();
-        if(prob > 0.5){
-          document.body.style = "background-color: blue";
+        else{
+            return "Gana Papel";
         }
-        else{console.log('red');
-        document.body.style = "background-color: red";
-
-      }
+    }
+    if(opcion1==="Papel"){
+        if(opcion2==="Piedra"){
+            return "Gana Papel";
+        }
+        else{
+            return "Gana Tijera";
+        }
+    }
+    if(opcion1==="Tijera"){
+        if(opcion2==="Piedra"){
+            return "Gana Piedra";
+        }
+        else{
+            return "Gana Tijera";
+        }
+    }
+};
+compara(eleccionUsuario,eleccionComputador);
